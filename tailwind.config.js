e5489f3ba298992adbb87e498adfe4d55b92c445
Augusto <augusto2.logo@gmail.com>
2026-07-11 16:37:06 -0300

    chore: reconfig everything, expo SDK 57 + nativeWind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // O "*" significa "tudo dentro de src", e "{...}" cobre os formatos comuns
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
