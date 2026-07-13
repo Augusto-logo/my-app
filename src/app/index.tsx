1ac98530b8aff4d3e7ff490ea45ee1a9404c9559
Augusto <augusto2.logo@gmail.com>
2026-07-13 15:34:54 -0300

    feat: adds prettier and ESLint

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,9 @@
 import { Text, View } from "react-native";
 import { Image } from "expo-image";
-import "../../global.css"
+import "../../global.css";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { useAssets } from "expo-asset";
+import AnimatedStyleUpdateExample from "@/components/AnimatedStyleUpdateExample";
 
 export default function Index() {
   const [assets, error] = useAssets([
@@ -10,26 +11,10 @@ export default function Index() {
   ]);
 
   return (
-      <SafeAreaView className="flex-1 items-center">
-        <View className="flex-1 items-center bg-white gap-4">
-          <Text className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni recusandae odio totam. Necessitatibus, sunt iure! Veniam voluptas numquam, cupiditate provident voluptatem eius rem iste illum, eum sint itaque modi!</Text>
-          <Image 
-          contentFit="cover"
-          style={{ width: 300, height: 300, borderRadius: 10 }}
-          source={require("@/assets/images/index/paisagem_1.jpg")}
-          className=""
-          />
-
-          <Image
-          contentFit="cover"
-          style={{ width: 300, height: 300, borderRadius: 10 }}
-          source={{uri: "https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"}}
-          className=""
-          />
-          
-          
-        </View>
-      </SafeAreaView>
-    
+    <SafeAreaView className="flex-1 items-center">
+      <View className="flex-1 w-full bg-white gap-4">
+        <AnimatedStyleUpdateExample />
+      </View>
+    </SafeAreaView>
   );
 }
