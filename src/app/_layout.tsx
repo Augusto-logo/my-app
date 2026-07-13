7b9907c506acf667be0066824b643ce674e4d818
Augusto <augusto2.logo@gmail.com>
2026-07-13 14:49:18 -0300

    feat: testing assets

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <>
-    <Stack />;
-    <StatusBar style="auto"  />
-  </>
+
+  return (
+    <SafeAreaProvider>
+      <Stack />
+    </SafeAreaProvider>
+  );
 }
